6c1b6a0e23f34e709ce134e7fe48ea8fb91fad57
doatnguyen <doat13cn6@gmail.com>
2020-07-04 06:50:38 +0700

    testing

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const path = require('path');
+const login = require("facebook-chat-api");
+const fs = require("fs");
 const PORT = process.env.PORT || 5000
 const bodyParser = require('body-parser');
 var app=express();
+var email;
+var password;
 
 app.use(express.static(path.join(__dirname, 'public')))
   .set('port', PORT)
@@ -15,9 +19,61 @@ app.use(bodyParser.json());
 
 // parse application/x-www-form-urlencoded 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.post('/test', function (req, res) {
+app.post('/send', function (req, res) {
+	var body = req.body;
+	var pageid= body.pageid;
+	var mess=body.mess;
+	login({appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8'))}, (err, api) => {
+		if(err) {
+			res.send(err);
+		}
+		console.log("Login!");
+		api.sendMessage(mess, pageid);
+		res.send("ok");
+	});
+});
+app.post('/login', function (req, res) {
   var body = req.body;
-  var email = body.email;
-  var password = body.password;
-  console.log(email+" "+password);
+  email= body.email;
+  password= body.password;
+  var credentials = {email: email, password: password};
+  console.log(credentials);
+  login(credentials, (err, api) => {
+		if(err) {
+			switch (err.error) {
+				case 'login-approval':
+					res.send("Need 2fa");
+                break;
+            default:
+                res.send(err);
+        }
+        return;
+		}
+		res.send("ok");
+		console.log("Login!");
+		fs.writeFileSync('appstate.json', JSON.stringify(api.getAppState()));
+	});
+});
+app.post('/login2fa', function (req, res) {
+  var body = req.body;
+  email= body.email;
+  password= body.password;
+  var code = body.code-fa;
+  var credentials = {email: email, password: password};
+  console.log(credentials);
+  login(credentials, (err, api) => {
+		if(err) {
+			switch (err.error) {
+				case 'login-approval':
+					err.continue(code);
+                break;
+            default:
+                res.send(err);
+        }
+        return;
+		}
+		res.send("ok");
+		console.log("Login!");
+		fs.writeFileSync('appstate.json', JSON.stringify(api.getAppState()));
+	});
 });
