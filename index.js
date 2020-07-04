efe23f864a30beb2b4e50e655cdc40f44e95debe
doatnguyen <doat13cn6@gmail.com>
2020-07-04 13:52:42 +0700

    get user on file

--- index.js
+++ index.js
@@ -14,11 +14,17 @@ app.use(express.static(path.join(__dirname, 'public')))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-// parse application/json 
+  
+  
 app.use(bodyParser.json());
-
-// parse application/x-www-form-urlencoded 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.get('/user_list', function (req, res) {
+	let data=[];
+	fromDir('../user',/\.json$/,function(filename){
+		data.push(filename);
+	});
+	res.send(data);
+});
 app.post('/send', function (req, res) {
 	var body = req.body;
 	var pageid= body.pageid;
@@ -76,4 +82,33 @@ app.post('/login2fa', function (req, res) {
 		console.log("Login!");
 		fs.writeFileSync('appstate.json', JSON.stringify(api.getAppState()));
 	});
+});
+
+
+
+//function
+
+
+function fromDir(startPath,filter,callback){
+
+    //console.log('Starting from dir '+startPath+'/');
+
+    if (!fs.existsSync(startPath)){
+        console.log("no dir ",startPath);
+        return;
+    }
+
+    var files=fs.readdirSync(startPath);
+    for(var i=0;i<files.length;i++){
+        var filename=path.join(startPath,files[i]);
+        var stat = fs.lstatSync(filename);
+        if (stat.isDirectory()){
+            fromDir(filename,filter,callback); //recurse
+        }
+        else if (filter.test(filename)) callback(filename);
+    };
+};
+
+fromDir('../LiteScript',/\.html$/,function(filename){
+    console.log('-- found: ',filename);
 });
